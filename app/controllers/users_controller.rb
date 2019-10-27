511a38a3befe5b0eb85323b2cd0a8c6368cdc985
sinigam1192 <yuie7893@gmail.com>
2019-10-27 18:06:53 +0900

    moblie

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,17 @@
 class UsersController < ApplicationController
+
+  def login
+  end
+
+  def user
+  end
+
+  def company
+  end
+
+  def mylist
+  end
+
+  def new_user
+  end
 end
--- app/controllers/work_controller.rb
+++ app/controllers/work_controller.rb
@@ -1,2 +1,13 @@
 class WorkController < ApplicationController
+  def new_company
+  end
+
+  def new_report
+  end
+
+  def report
+  end
+
+  def show_company
+  end
 end
