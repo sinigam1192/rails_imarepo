fd263afd14376578ebd6f21813a9eacf83e5391c
sinigam1192 <yuie7893@gmail.com>
2020-01-12 18:19:53 +0900

    heroku deploy 2nd
    change routes.eb root

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root "users#login"
+  root "users#login_form"
 #usersコントローラ
   post "/login" => "users#login"
   post "/users/create" => "users#create"
